b42c80a3c19fefe536db2e6a9d99444b66b9d091
taylor-desktop <yushkevich.kiryl@gmail.com>
2021-09-10 17:19:47 +0200

    - parallax effect

--- js/script.js
+++ js/script.js
@@ -59,3 +59,24 @@ class TabItem {
 document.addEventListener('DOMContentLoaded', ()=>{
   let tabs = new TabsManager(document.querySelector('.advantage'));
 })
+
+// parallax effect
+
+// Add event listener
+document.addEventListener("mousemove", parallax);
+const elem = document.querySelector(".advantage__contentItem");
+
+
+// Magic happens here
+function parallax(e) {
+  let _w = window.innerWidth/2;
+  let _h = window.innerHeight/2;
+  let _mouseX = e.clientX;
+  let _mouseY = e.clientY;
+  let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+  let _depth2 = `${50 - (_mouseX - _w) * 0.1}% ${50 - (_mouseY - _h) * 0.1}%`;
+  let _depth3 = `${50 - (_mouseX - _w) * -(0.07)}% ${50 - (_mouseY - _h) * -(0.07)}%`;
+  let x = `${_depth3}, ${_depth2}, ${_depth1}`
+  console.log(x);
+  elem.style.backgroundPosition = x;
+}
